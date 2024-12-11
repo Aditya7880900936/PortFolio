4f3d40facba1d8a056ac78e9ee1377bf362cd6e7
vanshguptaji <vansh23164033@akgec.ac.in>
2024-12-12 01:21:47 +0530

    Working on Navbar section and assests folder added

---
+++ PortFolio/src/App.jsx
@@ -0,0 +1,35 @@
+// import React from 'react'
+// import {Canvas} from '@react-three/fiber'
+// import {OrbitControls} from '@react-three/drei'
+// import RotatingCube from './Components/RotatingCube'
+
+// const App = () => {
+//   return (
+//     <Canvas style={{height : '100vh' , width : '100vw' , display : 'flex' ,
+//       justifyContent : "center" , alignItems : 'center' }}>
+//       <OrbitControls enableZoom enablePan enableRotate/>
+
+//       <directionalLight position={[1 , 1, 1]} intensity={10} color={0x9CDBA6}/>
+
+//       <color attach = "background" args = {['#F0F0F0']}/>
+
+//       <RotatingCube/>
+//     </Canvas>
+//   )
+// }
+
+// export default App
+
+import React from 'react'
+import Navbar from './Sections/Navbar'
+
+const App = () => {
+  return (
+    <main className='max-w-7xl mx-auto'>
+      <Navbar/>
+    </main>
+  )
+}
+
+export default App
+
