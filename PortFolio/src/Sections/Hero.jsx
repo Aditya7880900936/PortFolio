debc32e2f5a6569e86e5d76c1d1f9ff4fee0e08c
Aditya7880900936 <adityasanskarsrivastav788@gmail.com>
2024-12-14 00:57:02 +0530

    3d model is using

--- PortFolio/src/Sections/Hero.jsx
+++ PortFolio/src/Sections/Hero.jsx
@@ -1,5 +1,9 @@
-import React from "react";
+import React, { Suspense } from "react";
 import "../index.css";
+import { Canvas } from "@react-three/fiber";
+import { PerspectiveCamera } from "@react-three/drei";
+import HackerRoom from "../Components/HackerRoom";
+import CanvasLoader from "../Components/CanvasLoader";
 
 const Hero = () => {
   return (
@@ -9,6 +13,17 @@ const Hero = () => {
           Hello, I am Aditya <span className="waving-hand ">👋</span>
         </p>
         <p className="hero_tag text-gray_gradient">Building Products And Brands</p>
+      </div>
+
+      <div className="w-full h-full absolute inset-0">
+          <Canvas className="w-full h-full">
+            <Suspense fallback={<CanvasLoader/>}>
+            <PerspectiveCamera makeDefault position={[0, 0, 30]}/>
+            <HackerRoom scale={0.07} position={[0, 0, 0]} rotation={[0, 70, 0]}/>
+            <ambientLight intensity={1} />
+            <directionalLight position={[10, 10, 10]} intensity={0.5} />
+            </Suspense>
+          </Canvas>
       </div>
     </section>
   );
--- PortFolio/src/Sections/Navbar.jsx
+++ PortFolio/src/Sections/Navbar.jsx
@@ -1,24 +1,22 @@
 import React, { useState } from "react";
 import { navLinks } from "../constants";
 
-const NavItems = () => {
-  return (
-    <ul className="nav-ul">
-      {navLinks.map(({ id, name, href }) => (
-        <li key={id} className="nav-li">
-          <a href={href} className="nav-li_a" onClick={() => {}}>
-            {name}
-          </a>
-        </li>
-      ))}
-    </ul>
-  );
-};
+const NavItems = ()=>{
+      return(
+        <ul className="nav-ul">
+              {navLinks.map(({id,name,href}) => (
+                <li key={id} className="nav-li"> 
+                <a href={href} className="nav-li_a" onClick={()=>{}}>{name}</a>
+                </li>
+              ))}
+        </ul>
+      )
+}
 
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const[isOpen,setIsOpen] = useState(false);
 
-  const toggleMenu = () => setIsOpen((prevIsOpen) => !prevIsOpen);
+  const toggleMenu= () => setIsOpen((prevIsOpen) => !prevIsOpen)
 
   return (
     <header className="fixed top-0 left-0 right-0 z-50 bg-black/90 ">
@@ -27,31 +25,23 @@ const Navbar = () => {
           <a
             href="/"
             className="text-neutral-400 font-bold text-xl hover:text-white transition-colors"
-          >
-            {" "}
-            Aditya
-          </a>
-          <button
-            onClick={toggleMenu}
-            className="text-neutral-400 hover:text-white focus:outline-none sm:hidden flex"
-            aria-label="Toggle Menu"
-          >
-            <img
-              src={isOpen ? "assets/close.svg" : "assets/menu.svg"}
-              alt="toggle"
-              className="w-6 h-6"
-            />
+          > Aditya</a>
+          <button onClick={toggleMenu} className="text-neutral-400 hover:text-white focus:outline-none sm:hidden flex" aria-label="Toggle Menu">
+            <img src={ isOpen ? "assets/close.svg" : "assets/menu.svg"} alt="toggle" className="w-6 h-6" />
           </button>
+
           <nav className="sm:flex hidden">
-            <NavItems />
+              <NavItems/>
           </nav>
         </div>
       </div>
+
       <div className={`nav-sidebar ${isOpen ? "max-h-screen" : "max-h-0"}`}>
-        <nav className="p-5">
-          <NavItems />
-        </nav>
+          <nav className="p-5">
+              <NavItems/>
+          </nav>
       </div>
+      
     </header>
   );
 };
