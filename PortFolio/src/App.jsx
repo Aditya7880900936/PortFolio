35c081a8fb615fdba1ca6fe3eac2afe0407e7342
Aditya7880900936 <adityasanskarsrivastav788@gmail.com>
2024-12-14 00:53:59 +0530

    Working on Hero section in whcih a 3D model is integrated by the hero section

--- PortFolio/src/App.jsx
+++ PortFolio/src/App.jsx
@@ -22,11 +22,13 @@
 
 import React from 'react'
 import Navbar from './Sections/Navbar'
+import Hero from './Sections/Hero'
 
 const App = () => {
   return (
     <main className='max-w-7xl mx-auto'>
       <Navbar/>
+      <Hero/>
     </main>
   )
 }
