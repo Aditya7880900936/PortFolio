e12c33726bf22406eab01adcd01fe1b92c821b99
Aditya7880900936 <adityasanskarsrivastav788@gmail.com>
2024-12-21 23:28:30 +0530

    Right section of Project section is done by using a 3d model in it

--- PortFolio/src/Sections/Projects.jsx
+++ PortFolio/src/Sections/Projects.jsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { useState } from "react";
 import { myProjects } from "../constants";
+import { Canvas} from "@react-three/fiber";
+import { Center , OrbitControls } from "@react-three/drei";
+import DemoComputer from "../Components/DemoComputer";
+import CanvasLoader from "../Components/CanvasLoader";
 
 
 const projectsCount = myProjects.length;
@@ -20,7 +24,7 @@ const Projects = () => {
   }
 
   return (
-    <section className="c-space my-20 ">
+    <section className="c-space my-20" id="work">
       <p className="head-text">My Work</p>
       <div className="grid lg:grid-cols-2 grid-cols-1 mt-12 gap-5 w-full">
         <div className="flex flex-col gap-5 relative sm:p-10 py-10 px-5 shadow-2xl shadow-black-200">
@@ -69,6 +73,21 @@ const Projects = () => {
                 <img src="/assets/right-arrow.png" alt="right arrow" className="w-3 h-3" />
              </button>
           </div>
+        </div>
+
+        <div className="border border-black-300 bg-black-200 rounded-lg h-96 md:h-full">
+              <Canvas>
+                    <ambientLight intensity={Math.PI} />
+                    <directionalLight position = {[1,1,5]}/>
+                    <Center>
+                      <Suspense fallback={<CanvasLoader/>}>
+                            <group scale={1.8} position={[0,-3,0]} rotation={[0,-0.1,0]}>
+                               <DemoComputer texture={currentProject.texture}/>
+                            </group>
+                      </Suspense>
+                    </Center>
+                    <OrbitControls maxPolarAngle={Math.PI / 2} enableZoom={false}/>
+              </Canvas>
         </div>
       </div>
     </section>
